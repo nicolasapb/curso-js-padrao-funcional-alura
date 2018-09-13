188f7880027b5934aaecb0341021a52a96714cb2
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-12 21:52:58 -0300

    jpf 2.10

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,13 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { notasService as service } from './nota/service.js';
 
+        // .then(notas => notas.flatMap(nota => nota.itens)) //<-- Chrome 69
+        // .then(notas => notas.reduce((itens, nota) => itens.concat(nota.itens), []))
 document
     .querySelector('#myButton')
     .onclick = () =>
-        fetch('http://localhost:3000/notsas')
-        .then(handleStatus)
-        .then(notas => console.log(notas))
+        service
+        .sumItems('2143')
+        .then(console.log)
         .catch(console.log);
+
+
