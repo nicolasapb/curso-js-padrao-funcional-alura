ff2e77a3ea18e8cc8dfbeca384991d10d8f86404
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-14 08:42:22 -0300

    jpf 3.10

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,20 +1,29 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, compose, pipe } from '../utils/operators.js';
 import '../utils/array-helpers.js'
 
 const API = 'http://localhost:3000/notas';
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
 
     listAll() {
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as Notas Fiscais');
+            });
     },
 
     sumItems(code) {
-        return this.listAll().then(sumItems(code));
+
+        const filterItems = partialize(filterItemsByCode, code);
+        // const sumItems = compose(sumItemsValue, filterItems, getItemsFromNotas);
+        const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
+        return this.listAll().then(sumItems);
     }
 };
