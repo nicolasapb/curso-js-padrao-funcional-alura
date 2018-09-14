bfd01631bdddab7a9440af016751c61f2b14b374
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-14 10:33:35 -0300

    jpf 4.6

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,30 @@
 import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';
 
-        // .then(notas => notas.flatMap(nota => nota.itens)) //<-- Chrome 69
-        // .then(notas => notas.reduce((itens, nota) => itens.concat(nota.itens), []))
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+    service
+		.sumItems('2143')
+		.then(console.log)
+		.catch(console.log)
+);
+// const action = debounceTime(500,
+// 	takeUntil(3, () =>
+// 		service
+// 		.sumItems('2143')
+// 		.then(console.log)
+// 		.catch(console.log)
+// 	)
+// );
+
 document
-    .querySelector('#myButton')
-    .onclick = () =>
-        service
-        .sumItems('2143')
-        .then(console.log)
-        .catch(console.log);
+	.querySelector('#myButton')
+	.onclick = action;
 
 
+// .then(notas => notas.flatMap(nota => nota.itens)) //<-- Chrome 69
+// .then(notas => notas.reduce((itens, nota) => itens.concat(nota.itens), []))
