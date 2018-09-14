6dfcc4b8de72e10cd1cbf028c60edc352cb09388
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-14 12:02:41 -0300

    jpf 5.8

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,6 @@
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js';
+import { timeOutPromise, retry } from './utils/promise-helpers.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -7,19 +8,11 @@ const operations = pipe(
 );
 
 const action = operations(() =>
-    service
-		.sumItems('2143')
-		.then(console.log)
-		.catch(console.log)
+    retry(3, 3000, () => timeOutPromise(200, service.sumItems('2143')))
+    .then(console.log)
+    .catch(console.log)
 );
-// const action = debounceTime(500,
-// 	takeUntil(3, () =>
-// 		service
-// 		.sumItems('2143')
-// 		.then(console.log)
-// 		.catch(console.log)
-// 	)
-// );
+
 
 document
 	.querySelector('#myButton')
@@ -28,3 +21,11 @@ document
 
 // .then(notas => notas.flatMap(nota => nota.itens)) //<-- Chrome 69
 // .then(notas => notas.reduce((itens, nota) => itens.concat(nota.itens), []))
+// const action = debounceTime(500,
+// 	takeUntil(3, () =>
+// 		service
+// 		.sumItems('2143')
+// 		.then(console.log)
+// 		.catch(console.log)
+// 	)
+// );
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -4,3 +4,22 @@ export const log = param => {
     console.log(param);
     return param;
 }
+
+export const timeOutPromise = (ms, promise) => {
+
+    const timeOut = new Promise((_resolve, reject) =>
+        setTimeout(() => reject(`Timeout (${ms} ms)`), ms));
+
+    return Promise.race([timeOut, promise]);
+};
+
+export const delay = ms => data =>
+    new Promise((resolve, _reject) =>
+        setTimeout(() => resolve(data), ms));
+
+export const retry = (retries, ms, fn) =>
+    fn().catch(err => {
+        console.log(retries);
+        return delay(ms)().then(() =>
+            retries > 1 ? retry(--retries, ms, fn) : Promise.reject(err));
+    });
